20df9418068a4608f256e3e7dd74b4e3d7f0bdcc
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-08-13 21:09:22 +0800

    baru

--- app/Http/Livewire/Kegiatan/Index.php
+++ app/Http/Livewire/Kegiatan/Index.php
@@ -22,8 +22,8 @@ class Index extends Component
   {
     return view('livewire.kegiatan.index', [
       'data' => Posting::where('jenis', 'kegiatan')->when($this->arsip, fn($q) => $q->where('created_at', 'like', $this->arsip . '%'))->when($this->kategori, fn($q) => $q->where('kategori_id', $this->kategori))->orderBy('created_at', 'desc')->paginate(3),
-      'terpopuler' => Posting::withCount('hit')->orderBy('hit_count', 'desc')->take(5)->get(),
-      'dataKategori' => Kategori::withCount('posting')->orderBy('posting_count', 'desc')->take(5)->get(),
+      'terpopuler' => Posting::where('jenis', 'kegiatan')->withCount('hit')->orderBy('hit_count', 'desc')->take(5)->get(),
+      'dataKategori' => Kategori::where('jenis', 'kegiatan')->withCount('posting')->whereHas('posting', fn($q) => $q->where('jenis', 'kegiatan'))->orderBy('posting_count', 'desc')->take(5)->get(),
       'dataArsip' => Posting::select(DB::raw('left(created_at,7) as tanggal'))->groupBy(DB::raw('left(created_at,7)'))->orderBy('tanggal', 'asc')->get(),
     ])
       ->extends('layouts.app');
