4246510eaeaa3f43db459f6ed57009f7a75908c6
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-08-28 11:19:37 +0800

    judul kategori?

--- resources/views/livewire/adminarea/datamaster/kategori/form.blade.php
+++ resources/views/livewire/adminarea/datamaster/kategori/form.blade.php
@@ -6,13 +6,13 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Pengumuman</h1>
+            <h1 class="m-0">Data Master <small>Kategori</small></h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item">Pengumuman</li>
-              <li class="breadcrumb-item active">{{ $key ? 'Edit' : 'Tambah' }}</li>
+              <li class="breadcrumb-item">Data Master</li>
+              <li class="breadcrumb-item active">Kategori {{ $key ? 'Edit' : 'Tambah' }}</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
